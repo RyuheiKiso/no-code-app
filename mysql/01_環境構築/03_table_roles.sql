3bbad660959f2d9caf5025812604f882f0017954
kiso ryuhei <uw2374@icloud.com>
2024-12-05 21:56:23 +0900

    rolesテーブルをcm_m_rolesにリネームし、関連するSQLスクリプトを更新

--- mysql/01_環境構築/03_table_roles.sql
+++ mysql/01_環境構築/03_table_roles.sql
@@ -2,10 +2,10 @@
 USE sample;
 
 -- 既存のテーブルを削除
-DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS cm_m_roles;
 
 -- 権限テーブル
-CREATE TABLE roles (
+CREATE TABLE cm_m_roles (
     role_id INT PRIMARY KEY,
     role_name VARCHAR(50) NOT NULL
 );
