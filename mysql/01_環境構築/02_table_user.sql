86ab2d374e2e1d7d5202a6927c1e212a2d4f94dc
kiso ryuhei <uw2374@icloud.com>
2024-12-05 21:48:40 +0900

    ユーザーテーブルのコメントを更新し、権限コードを削除

--- mysql/01_環境構築/02_table_user.sql
+++ mysql/01_環境構築/02_table_user.sql
@@ -1,7 +1,7 @@
 -- データベースを選択
 USE sample;
 
--- テーブルを作成
+-- ユーザーテーブル
 CREATE TABLE cm_t_users (
     id INT AUTO_INCREMENT PRIMARY KEY COMMENT 'ユーザーID',
     first_name VARCHAR(50) NOT NULL COMMENT '名',
@@ -9,7 +9,6 @@ CREATE TABLE cm_t_users (
     email VARCHAR(100) NOT NULL COMMENT 'メールアドレス',
     password VARCHAR(255) NOT NULL COMMENT 'パスワード',
     last_login TIMESTAMP NULL COMMENT '最終ログイン日時',
-    permission_code VARCHAR(20) NULL COMMENT '権限コード',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '作成日時',
     created_by VARCHAR(50) NOT NULL COMMENT '作成ユーザー',
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日時',
