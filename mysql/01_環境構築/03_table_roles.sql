98016eccf0e18f75cbdeacdaef54fd0d9684acf8
kiso ryuhei <uw2374@icloud.com>
2024-12-05 22:02:00 +0900

    cm_m_rolesテーブルに作成日時、作成ユーザー、更新日時、更新ユーザーのカラムを追加

--- mysql/01_環境構築/03_table_roles.sql
+++ mysql/01_環境構築/03_table_roles.sql
@@ -7,5 +7,9 @@ DROP TABLE IF EXISTS cm_m_roles;
 -- 権限テーブル
 CREATE TABLE cm_m_roles (
     role_id INT PRIMARY KEY,
-    role_name VARCHAR(50) NOT NULL
+    role_name VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '作成日時',
+    created_by VARCHAR(50) NOT NULL COMMENT '作成ユーザー',
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日時',
+    updated_by VARCHAR(50) NOT NULL COMMENT '更新ユーザー'
 );
