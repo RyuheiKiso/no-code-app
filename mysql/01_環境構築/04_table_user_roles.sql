22acdbcce42b39535edfdde12bfbc60fb64545d1
kiso ryuhei <uw2374@icloud.com>
2024-12-05 21:57:04 +0900

    user_rolesテーブルをcm_m_user_rolesにリネームし、関連する外部キーを更新

--- mysql/01_環境構築/04_table_user_roles.sql
+++ mysql/01_環境構築/04_table_user_roles.sql
@@ -2,13 +2,13 @@
 USE sample;
 
 -- 既存のテーブルを削除
-DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS cm_m_user_roles;
 
 -- ユーザーと権限の関係テーブル
-CREATE TABLE user_roles (
+CREATE TABLE cm_m_user_roles (
     user_id INT,
     role_id INT,
     PRIMARY KEY (user_id, role_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (role_id) REFERENCES roles(role_id)
+    FOREIGN KEY (user_id) REFERENCES cm_m_users(user_id),
+    FOREIGN KEY (role_id) REFERENCES cm_m_roles(role_id)
 );
