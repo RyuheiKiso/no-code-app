34e0ce1a76e7b1cd9dbad83b17412fc73c860fc4
kiso ryuhei <uw2374@icloud.com>
2024-12-05 21:53:36 +0900

    ユーザーと権限の関係テーブル'user_roles'を作成するSQLスクリプトを追加

---
+++ mysql/01_環境構築/04_table_user_roles.sql
@@ -0,0 +1,11 @@
+-- データベースを選択
+USE sample;
+
+-- ユーザーと権限の関係テーブル
+CREATE TABLE user_roles (
+    user_id INT,
+    role_id INT,
+    PRIMARY KEY (user_id, role_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (role_id) REFERENCES roles(role_id)
+);
