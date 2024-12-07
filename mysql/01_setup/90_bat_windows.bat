fa262fd6adefbbbfad9d2493267568e00ab09d83
kiso ryuhei <uw2374@icloud.com>
2024-12-08 00:31:10 +0900

    MySQL環境構築用のバッチファイルを追加

---
+++ mysql/01_setup/90_bat_windows.bat
@@ -0,0 +1,20 @@
+@echo off
+
+REM MySQLコンテナを起動
+docker run --name mysql_container -e MYSQL_ROOT_PASSWORD=rootpassword -e MYSQL_DATABASE=sample -e MYSQL_USER=dev -e MYSQL_PASSWORD=dev -p 3306:3306 -d mysql:latest
+
+REM MySQLコンテナが起動するまで待機
+timeout /t 30
+
+REM MySQLコンテナに接続してスクリプトを実行
+docker exec -i mysql_container mysql -u root -prootpassword < mysql/01_setup/00_create_user.sql
+docker exec -i mysql_container mysql -u root -prootpassword < mysql/01_setup/01_create_database.sql
+docker exec -i mysql_container mysql -u root -prootpassword < mysql/01_setup/02_table_user.sql
+docker exec -i mysql_container mysql -u root -prootpassword < mysql/01_setup/03_table_roles.sql
+docker exec -i mysql_container mysql -u root -prootpassword < mysql/01_setup/04_table_user_roles.sql
+docker exec -i mysql_container mysql -u root -prootpassword < mysql/01_setup/05_table_menu.sql
+docker exec -i mysql_container mysql -u root -prootpassword < mysql/01_setup/06_table_menu_permissions.sql
+docker exec -i mysql_container mysql -u root -prootpassword < mysql/01_setup/07_table_log.sql
+
+echo 環境構築が完了しました。
+pause
