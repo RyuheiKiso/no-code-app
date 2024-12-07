fd0b0f915df1482fa0277e607e0760b2c0286a72
kiso ryuhei <uw2374@icloud.com>
2024-12-07 22:43:46 +0900

    cm_m_rolesテーブルにサンプルデータを挿入

--- mysql/01_setup/03_table_roles.sql
+++ mysql/01_setup/03_table_roles.sql
@@ -13,3 +13,9 @@ CREATE TABLE cm_m_roles (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日時',
     updated_by VARCHAR(50) NOT NULL COMMENT '更新ユーザー'
 );
+
+-- サンプルデータを挿入
+INSERT INTO cm_m_roles (role_id, role_name, created_by, updated_by)
+VALUES (1, 'Admin', 'system', 'system'),
+       (2, 'User', 'system', 'system'),
+       (3, 'Guest', 'system', 'system');
