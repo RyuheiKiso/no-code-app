672d66e46f3440c878702e44d06fc2d4e102e5cf
kiso ryuhei <uw2374@icloud.com>
2024-12-09 21:59:20 +0900

    メッセージ定数を追加し、エラーメッセージ、成功メッセージ、バリデーションエラーメッセージを定義

---
+++ web_react/src/shared/constants/messages.tsx
@@ -0,0 +1,15 @@
+// messages.tsx
+
+// エラーメッセージ
+export const ERROR_MESSAGE = 'エラーが発生しました。';
+
+// 成功メッセージ
+export const SUCCESS_MESSAGE = '操作が成功しました。';
+
+// バリデーションエラーメッセージ
+export const VALIDATION_ERROR_MESSAGE = '入力内容を確認してください。';
+
+// 使用例:
+// console.log(ERROR_MESSAGE);
+// console.log(SUCCESS_MESSAGE);
+// console.log(VALIDATION_ERROR_MESSAGE);
